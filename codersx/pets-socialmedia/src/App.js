adff6c3d12ebfd61c57c2b8501c51f813b8d8dde
hieuk3sh48 <pvhieu7795@gmail.com>
2020-08-08 10:36:29 +0700

    pet-projects

---
+++ codersx/pets-socialmedia/src/App.js
@@ -0,0 +1,221 @@
+import React, { useState, useEffect } from 'react';
+import { Modal, makeStyles, Input, Button } from '@material-ui/core';
+import InstagramEmbed from 'react-instagram-embed';
+
+import './App.css';
+import Post from './Post';
+import {db,auth} from './firebase';
+import ImageUpload from './ImageUpload';
+
+function getModalStyle(){
+  const top = 50 ;
+  const left = 50 ;
+
+  return{
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`
+  }
+}
+
+const useStyles = makeStyles(theme=>({
+  paper:{
+    position:'absolute',
+    width:400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2,4,3)
+  }
+}))
+
+function App() {
+  const [posts, setPosts]=useState([]);
+  const [openSignUp, setOpenSignUp] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null);
+
+  const [modalStyle] = useState(getModalStyle)
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(authUser=>{
+      if(authUser){
+        //user has logged in...
+        setUser(authUser);
+      }else{
+        // user has logged out...
+        setUser(null)
+      }
+    })
+
+    return ()=>{
+      unsubscribe();
+    }
+  },[user,username])
+  useEffect(() => {
+
+    db.collection('posts')
+    .orderBy('timestamp','desc')
+    .onSnapshot(snapshot=>{
+
+      setPosts(snapshot.docs.map(doc=>({id:doc.id, data:doc.data()})))
+    })
+  }, [])
+
+  const signUp=(event)=>{
+    event.preventDefault();
+    //Xac thuc user
+    auth
+    .createUserWithEmailAndPassword(email,password)
+    .then(authUser=>{
+
+      return authUser.user.updateProfile({
+        displayName: username
+      })
+    })
+    .catch(err=>alert(err.message))
+
+    setOpenSignUp(false);
+  }
+  
+  const signIn=(event)=>{
+    event.preventDefault();
+
+    auth
+    .signInWithEmailAndPassword(email,password)
+    .catch(err=>alert(err.message))
+
+    setOpenSignIn(false);
+  }
+
+  return (
+    <div className="App">
+
+
+      <Modal
+        open={openSignUp}
+        onClose={()=> setOpenSignUp(false)}  
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className='app__signup'>
+            <center>
+              <img 
+                className='app__headerImage'
+                alt='logo'
+                src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+              />
+            </center>
+              <Input
+                type='text'
+                placeholder='username'
+                value={username}
+                onChange={e=>setUsername(e.target.value)}
+              />
+              <Input
+                type='email'
+                placeholder='email'
+                value={email}
+                onChange={e=>setEmail(e.target.value)}
+              />
+              <Input
+                type='password'
+                placeholder='password'
+                value={password}
+                onChange={e=>setPassword(e.target.value)}
+              />
+              <Button type='submit' onClick={signUp}>Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
+      <Modal
+        open={openSignIn}
+        onClose={()=> setOpenSignIn(false)}  
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className='app__signup'>
+            <center>
+              <img 
+                className='app__headerImage'
+                alt='logo'
+                src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+              />
+            </center>
+              <Input
+                type='email'
+                placeholder='email'
+                value={email}
+                onChange={e=>setEmail(e.target.value)}
+              />
+              <Input
+                type='password'
+                placeholder='password'
+                value={password}
+                onChange={e=>setPassword(e.target.value)}
+              />
+              <Button type='submit' onClick={signIn}>Sign In</Button>
+          </form>
+        </div>
+      </Modal>
+
+
+      <div className='app__header'>
+          <img 
+            className='app__headerImage'
+            alt='logo'
+            src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+          />
+
+          {
+            user?(
+              <Button onClick={()=>auth.signOut()}>Logout</Button>
+            ):(
+              <div className='app__loginContainer'>
+                <Button onClick={()=>setOpenSignIn(true)}>Sign In</Button>
+                <Button onClick={()=>setOpenSignUp(true)}>Sign Up</Button>
+              </div>
+            )
+          }
+
+      </div>
+      
+      <div className='app__posts'>
+        <div className='left'>
+          {
+            user && posts.map(post=>(
+              <Post key={post.id} username={user.displayName} id={post.id} post={post.data}/>
+            ))
+          }
+        </div>
+        <div className='right'>
+          <InstagramEmbed
+            url='https://www.instagram.com/p/CB30zCNpSQm/'
+            maxWidth={320}
+            hideCaption={false}
+            containerTagName='div'
+            protocol=''
+            injectScript
+            onLoading={() => {}}
+            onSuccess={() => {}}
+            onAfterRender={() => {}}
+            onFailure={() => {}}
+          />
+        </div>
+      </div>
+  
+      {
+        user?.displayName?(
+          <ImageUpload username={user.displayName}/>
+        ):(
+          <h3>Sorry you need to login to upload</h3>
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ codersx/typing-speed-test/src/components/Form.js
@@ -0,0 +1,242 @@
+import React, { useState, useEffect } from 'react';
+import './Form.css';
+
+/* 
+    array: là mảng chữ mẫu để mình viết theo.
+    arrayTyping: là mảng chúng ta gõ được
+    currentWord: là vị trí chữ hiện tại chúng ta đang gõ, để khi chúng ta gõ đến đâu thì sẽ thêm css cho chữ mẫu vị trí đó để biết chúng ta gõ đến từ nào nhé.
+    countWordCorrect: đếm số từ đúng
+    countWordWrong: đếm số từ sai
+    timeCount: để check thời gian khi chúng ta gõ ký tự đầu tiên bộ đếm thời gian sẽ chạy
+*/
+
+function Form() {
+    const array = [
+        "công", "nghệ", "tay", "đơn", "viết", "lách", "lịch", "sử", "cảm", "ngoài", "hà", "hoàn", "toàn", "khỏe", "mạnh",
+        "cảm", "ơn", "dũng", "xã", "hội", "thanh", "bánh", "kính", "dạ", "vâng", "da", "thanh", "niên", "tỉnh", "tháng",
+        "hãy", "kính", "hạnh", "phúc", "ngữ", "văn", "khoa", "sinh", "hà", "nội", "màu", "ngành", "khó", "khăn", "trường",
+        "học", "quảng", "nam", "thành", "đỏ", "hãy", "sư", "phạm", "làm", "giao", "thông", "thang", "chú", "gì", "kĩ",
+        "thuật", "xin", "lỗi", "gió", "tuổi", "tin", "ngoài", "tri", "tháng", "phủ", "hoàn", "toàn", "phải", "chính",
+        "khóc", "ngày", "phố", "quảng", "nam", "tự", "do", "minh", "khỏe", "mạnh", "ba", "bà", "đợi", "chi", "dũng",
+        "nhà", "sợ", "lao", "xao", "hay", "tin", "năm", "tri", "hạnh", "phúc", "đàn", "sáo", "phối", "hợp", "năm",
+        "xây", "dựng", "im", "giấy", "tờ", "trời", "chia", "sẻ", "nga", "nhung", "cười", "chú", "trong", "thi",
+        "hành", "cổ", "tích", "in", "tự", "do", "phương", "vì", "xin", "bao", "quát", "xin", "lỗi", "lại", "đi"
+    ]
+    const [arrayTyping, setArrayTyping] = useState([]);
+    const [currentWord, setCurrentWord] = useState(0);
+    const [time, setTime] = useState(60);
+    const [countWordCorrect, setCountWordCorrect] = useState(0);
+    const [countWordWrong, setCountWordWrong] = useState(0);
+    const [isTimeCount, setIsTimeCount] = useState(false);
+    //const [fishi, setFishi] = useState(false)
+    let element = array.map((element, index) =>
+        <span 
+            key={index} 
+            className={index === currentWord ? "highlight" : index < currentWord &&
+                       arrayTyping.map((typing,indexTyping)=>
+                           //console.log(array[1])
+                           typing === array[indexTyping] ? ' correct' : ' wrong'
+                       )
+                
+            }
+        >
+            {element + " "}
+        </span>
+    );
+
+    useEffect(() => {
+       if(isTimeCount){
+
+            let count = 0;
+            let timeWord = time ;
+
+            let timer = setInterval(()=>{
+                count = count + 1;
+                if(count <= 60){
+                    setTime(timeWord - count);
+                    
+                    if(count === 60 || !isTimeCount){
+                        clearInterval(timer);
+                        setIsTimeCount(false);
+                        //setFishi(true)
+                        //bbb();
+                        //alert('Hoàn thành');
+                        //reset();
+                    }
+                }             
+            },100) 
+       }
+    }, [isTimeCount])
+
+    /* const bbb = () => {
+
+        //console.log('ddddd')
+        console.log(arrayTyping)
+        element = array.map((element, indexElement) =>
+            <span 
+                key={indexElement} 
+                className={
+                    arrayTyping.map((typing,indexTyping)=>{
+
+                        if(element === typing) return 'correct'
+                        if(element !== typing) return 'wrong'
+                    })
+                }
+            >
+                {element + " "}
+            </span>
+        );
+
+    } */
+
+   /*  useEffect(() => {
+
+        if(fishi){
+            console.log('ddddd')
+            element = array.map((element, indexElement) =>
+                <span 
+                    key={indexElement} 
+                    className={
+                        arrayTyping.map((typing,indexTyping)=>{
+
+                            if(element === typing) return 'correct'
+                            if(element !== typing) return 'wrong'
+                        })
+                    }
+                >
+                    {" "}
+                </span>
+            );
+        }
+    }, [fishi]) */
+
+  /*   const handleStartTyping = () => {
+        console.log(isTimeCount)
+        let count = 0;
+        let timeWord = time ;
+
+        let timer = setInterval(()=>{
+            count = count + 1;
+            if(count <= 60){
+                setTime(timeWord - count);
+                
+                if(count === 60){
+                    handleClearInterval(timer)
+                    setIsTimeCount(false);
+                }
+            }             
+        },100) 
+    } */
+
+    /* const handleClearInterval = (timer) => {
+        clearInterval(timer);
+        handleCompare();
+    }
+ */
+    useEffect(() => {
+        
+        //console.log(arrayTyping)
+        let correct = 0;
+        let wrong = 0;
+        arrayTyping.map((item,index)=>{
+            //console.log(item, array[index])
+            if(item === array[index]){
+                correct = correct + 1
+            }
+            if(item !== array[index] && item){
+                wrong = wrong + 1
+            }
+        })
+
+        setCountWordCorrect(correct);
+        setCountWordWrong(wrong);
+    }, [arrayTyping])
+
+   /*  const handleCompare = () => {
+        console.log(arrayTyping)
+        arrayTyping.map((item,index)=>{
+            //console.log(item, array[index])
+            if(item === array[index]){
+                setCountWordCorrect(countWordCorrect + 1);
+            }
+        })
+
+        setCountWordWrong(arrayTyping.length - countWordCorrect);
+    } */
+
+    const handleChange = (event) => {
+        event.preventDefault();
+
+        let txtText = event.target.value;
+        let results = txtText.split(/\s+/).join(' ');
+       /*  let valueTimeCount ;
+        if(txtText.length === 1) valueTimeCount = true */
+
+        setArrayTyping(results.split(" "))
+        setCurrentWord(results.split(" ").length - 1)
+        setIsTimeCount((txtText.length === 1) && true);
+
+        //if(valueTimeCount) handleStartTyping();
+    }
+
+    const resetButton = (event) => {
+        event.preventDefault();
+
+        reset();
+    }
+
+    const reset = () => {
+        setIsTimeCount(false);
+        setArrayTyping([]);
+        setCurrentWord(0);
+        setTime(60);
+        setCountWordCorrect(0);
+        setCountWordWrong(0);
+    }
+
+    return (
+        <div className="container">
+            <div className="panel panel-primary panel-typing">
+                <div className="panel-heading">
+                    <h3>Website kiểm tra tốc độ gõ bàn phím</h3>
+                </div>
+                <div className="panel-body">
+                    <div className="item-character">
+                        {element}
+                    </div>
+                    <div className="row">
+                        <div className="col-md-6">
+                            <label>Input</label><br />
+                            <input 
+                                className="form-control"
+                                placeholder="Please input typing"
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div className="col-md-2">
+                            <label>Time</label><br />
+                            <button 
+                                className="btn btn-primary btn-lange" 
+                                onClick={resetButton}
+                            >
+                                <p id="show-time">
+                                    {time}
+                                </p>
+                            </button>
+                        </div>
+                        <div className="col-md-2">
+                            <label>Word correct</label>
+                            <p>{countWordCorrect}</p>
+                        </div>
+                        <div className="col-md-2">
+                            <label>Word wrong</label>
+                            <p>{countWordWrong}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Form
